36da874c8a06e37144ef58db41746829a7e142db
Admin <fc2002@bk.ru>
2019-10-20 21:51:53 +0300

    флагшток

---
+++ prog1.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()
+
+size = width, height = 300, 300
+screen = pygame.display.set_mode(size)
+
+
+def draw():
+    rect_color = pygame.Color('Brown')
+    rect_width = 0
+    rect_point = ((80, 20), (8, 250))
+    pygame.draw.rect(screen, rect_color, rect_point, rect_width)
+
+
+draw()
+
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.flip()
+
+pygame.quit()
